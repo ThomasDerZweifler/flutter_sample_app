5a358e5ee8bde1b74896751f9fc365dafe3eb5ec
thofu <thomasfunke71@gmail.com>
2024-12-03 09:20:34 +0100

    web view youtube autoplay

--- lib/page/youtube/start.dart
+++ lib/page/youtube/start.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -11,6 +13,43 @@ class YoutubePlayerPage extends StatefulWidget {
   State<YoutubePlayerPage> createState() => _MyYoutubePlayerPageState();
 }
 
+getHtml(videoId) {
+  return '''
+        <html>
+        
+            <body style="margin:0px;padding:0px;">
+               <div id="player"></div>
+                <script>
+                    var player;
+                    function onYouTubeIframeAPIReady() {
+                      player = new YT.Player('player', {
+                        videoId: '$videoId',
+                        width: '100%',
+                        playerVars: {
+                          'playsinline': 1
+                        }
+                      });
+                    }
+                    function seekTo(time) {
+                      player.seekTo(time, true);
+                    }
+                    function playVideo() {
+                      player.playVideo();
+                    }
+                    function pauseVideo() {
+                      player.pauseVideo();
+                    }
+                </script>
+                <script src="https://www.youtube.com/iframe_api"></script>
+            </body>
+        </html>
+    ''';
+}
+
+startVideo(WebViewController controller) async {
+  controller.runJavaScript('''playVideo();'''); // did not work
+}
+
 class _MyYoutubePlayerPageState extends State<YoutubePlayerPage> {
   var loadingPercentage = 0;
   late final WebViewController controller;
@@ -20,10 +59,9 @@ class _MyYoutubePlayerPageState extends State<YoutubePlayerPage> {
     super.initState();
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(
-        Uri.parse('https://www.youtube.com/watch?v=bHQqvYy5KYo'),
-      );
-
+      //..loadRequest(Uri.parse('https://www.youtube.com/watch?v=bHQqvYy5KYo'),);
+      //..loadFlutterAsset('assets/youtube.html');
+      ..loadHtmlString(getHtml("bHQqvYy5KYo"));
     if (!kIsWeb) {
       // none supported by web
       controller.setNavigationDelegate(NavigationDelegate(
@@ -44,6 +82,9 @@ class _MyYoutubePlayerPageState extends State<YoutubePlayerPage> {
         },
       ));
     }
+    Timer(Duration(seconds: 3), () {
+      startVideo(controller);
+    });
   }
 
   @override
@@ -53,10 +94,7 @@ class _MyYoutubePlayerPageState extends State<YoutubePlayerPage> {
           // TRY THIS: Try changing the color here to a specific color (to
           // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
           // change color while the other colors stay the same.
-          backgroundColor: Theme
-              .of(context)
-              .colorScheme
-              .inversePrimary,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
